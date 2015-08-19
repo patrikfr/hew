32c3234341bc4ea7eb4aeb02305219c5062e85a1
Patrik Fredriksson <patrikfr@gmail.com>
2015-08-19 23:47:41 +0200

    Licens now actually match the LICENSE file...

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject hew "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License (MIT)"
+            :url  "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.owfs/jowfsclient "1.2.6"]]
   :main ^:skip-aot hew.core
