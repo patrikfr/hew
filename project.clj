2037c2d60d38714f5c47661ffc9106acd92c9d5d
patrikfr Patrik Fredriksson <patrikfr@gmail.com>
2016-08-06 11:50:54 +0200

    Updated deps

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "The MIT License (MIT)"
             :url  "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.owfs/jowfsclient "1.2.6"]
-                 [clj-http "2.0.0"]
-                 [cheshire "5.5.0"]]
+                 [clj-http "2.2.0"]
+                 [cheshire "5.6.3"]]
   :main ^:skip-aot hew.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
