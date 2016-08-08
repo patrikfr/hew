51c2eb67d014dcfb0774b5b9426b37d74e9056ea
patrikfr Patrik Fredriksson <patrikfr@gmail.com>
2016-08-09 00:30:33 +0200

    Cleaned up.

--- src/hew/tempmap.clj
+++ src/hew/tempmap.clj
@@ -3,7 +3,7 @@
 (defn to-color
   "Gives the corresponding xy color for given temperature reading"
   [temp]
-  (condp #(<= (get %1 0) %2 (get %1 1)) temp
+  (condp #(<= (%1 0) %2 (%1 1)) temp
     [0 4] [0.139, 0.081]                                    ;blue
     [5 10] [0.6531, 0.2834]                                 ;red
     [0.3402, 0.356]))                                       ;beige
