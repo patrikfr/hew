a2344b3589ffb842f7225c822fe87e2aa4c14211
Patrik Fredriksson <patrikfr@gmail.com>
2017-01-05 00:31:23 +0100

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -6,9 +6,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.owfs/jowfsclient "1.2.6"]
-                 [clj-http "2.2.0"]
+                 [clj-http "2.3.0"]
                  [cheshire "5.6.3"]
-                 [jarohen/chime "0.1.9"]]
+                 [jarohen/chime "0.2.0"]]
   :main ^:skip-aot hew.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
