6d049e91c9bd3505f5a358e9e7ef6d39df364cd1
Patrik Fredriksson <patrikfr@gmail.com>
2021-04-27 18:43:46 +0200

    Cleaned up code a bit. Now uses 'environ' for config and can use either Hue temp sensor or 1-wire.

--- src/hew/core.clj
+++ src/hew/core.clj
@@ -10,9 +10,51 @@
   (:import (java.time Instant Duration))
   (:gen-class))
 
+(defn update-light [temp hue-host hue-user-id hue-light-name]
+  (let [rgb (tempmap/to-color temp)
+        xy (color/rgb-to-xy rgb)
+        light-id (hue/light-by-name hue-host hue-user-id hue-light-name)]
+    (println "Updating light" light-id "with xy" xy "for temp:" temp)
+    (hue/update-light-color! hue-host hue-user-id light-id xy)))
+
+(defn read-temp
+  "Read temp and update the light"
+  ([hue-host hue-user-id hue-sensor-id]
+   (:temperature (hue/read-temperature hue-host hue-user-id hue-sensor-id)))
+  ([conn owfs-sensor-id]
+   (owfs/read-temp conn owfs-sensor-id)))
+
+(defn -main
+  "Main function, regularly polls the temperature and update the light"
+  [& args]
+  (println "Init")
+  (println "Use OWFS? " (env :use-owfs))
+
+  (let [use-owfs (edn/read-string (env :use-owfs))
+        owfs-host (env :owfs-host)
+        owfs-port (edn/read-string (env :owfs-port))
+        owfs-sensor-id (env :owfs-sensor-id)
+        hue-host (env :hue-host)
+        hue-user-id (env :hue-user-id)
+        hue-light-name (env :hue-light-name)
+        hue-sensor-id (env :hue-sensor-id)]
+
+    (chime/chime-at
+      (chime/periodic-seq (Instant/now) (Duration/ofMinutes 5))
+      (fn [_] (let [temp (if use-owfs
+                           (read-temp (owfs/connect owfs-host owfs-port) owfs-sensor-id)
+                           (read-temp hue-host hue-user-id hue-sensor-id))]
+                (update-light temp hue-host hue-user-id hue-light-name))))))
+;(-main)
+
+
+
+
+;;-- Utility functions for REPL and dev use
+
 (defn owfs-ops []
   "OWFS ops for REPL use and development"
-  (let [conn (owfs/connect (env :owfs-host) (env :owfs-port))
+  (let [conn (owfs/connect (env :owfs-host) (edn/read-string (env :owfs-port)))
         temp (owfs/read-temp conn (env :owfs-sensor-id))]
     (println "Temp: " temp)))
 
@@ -22,31 +64,6 @@
         user-id (env :hue-user-id)
         sensor-id (env :hue-sensor-id)]
     (println "Lights: " (hue/lights host user-id))
-    (if (sensor-id) (println "Temp: " (:temperature (hue/read-temperature host user-id sensor-id))))
+    (if sensor-id (println "Temp: " (:temperature (hue/read-temperature host user-id sensor-id))))
     ;(println (hue/light-by-name host user-id "Bloom!"))
     ))
-
-(defn read-and-update [conn]
-  "Read temp and update the light"
-  (let [hue-host (env :hue-host)
-        hue-user-id (env :hue-user-id)
-        temp (if (edn/read-string (env :use-owfs))
-               (owfs/read-temp conn (env :owfs-sensor-id))
-               (:temperature (hue/read-temperature hue-host hue-user-id (env :hue-sensor-id))))
-        rgb (tempmap/to-color temp)
-        xy (color/rgb-to-xy rgb)
-        light-id (hue/light-by-name hue-host hue-user-id (env :hue-light-name))]
-    (println "Updating light" light-id "with xy" xy "for temp:" temp)
-    (hue/update-light-color! hue-host hue-user-id light-id xy)))
-
-(defn -main
-  "Main function, regularly polls the temperature and upate the light"
-  [& args]
-  (println "Init")
-  (println "Use OWFS? " (env :use-owfs))
-
-  (chime/chime-at
-    (chime/periodic-seq (Instant/now) (Duration/ofMinutes 5))
-    (fn [_] (read-and-update (owfs/connect (env :owfs-host) (edn/read-string (env :owfs-port))))))) ;TODO: Figure out which temp sensor to use here (OWFS vs Hue)
-
-;(-main)
--- src/hew/owfs.clj
+++ src/hew/owfs.clj
@@ -3,9 +3,9 @@
   (:import [org.owfs.jowfsclient OwfsConnectionFactory]))
 
 (defn connect
-  "Create a new thread-safe connection to OW Server using supplied connection spec:  e.g. (connect {:host \"localhost\", :port 2048}"
+  "Create a new thread-safe connection to OW Server using supplied connection spec:  e.g. (connect \"localhost\", 2048)"
   [host port]
-  (.createNewConnection (OwfsConnectionFactory. host (int port))))
+  (.createNewConnection (OwfsConnectionFactory. host port)))
 
 (defn read-temp [ow-conn sensor-id]
   (let [path (str "/" sensor-id "/temperature")
